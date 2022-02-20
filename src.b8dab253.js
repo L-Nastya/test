af0bc16153b8787e105937130825c7974f0f07ff
L-Nastya <94284760+L-Nastya@users.noreply.github.com>
2022-02-20 19:33:34 +0000

    Deploying to gh-pages from @ L-Nastya/test@34e2ae840bb94992cef9ee82433334d1eefc523a 🚀

--- src.b8dab253.js
+++ src.b8dab253.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/check.svg":[["check.e495c813.svg","YHTB"],"YHTB"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/test/src.75574720.js.map
+//# sourceMappingURL=/test/src.b8dab253.js.map
